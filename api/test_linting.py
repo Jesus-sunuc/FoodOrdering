6f6a09527f568b9eca52d38ec18100d279d94cc7
Jesus-sunuc <74077822+Jesus-sunuc@users.noreply.github.com>
2025-03-26 16:31:58 -0600

    added black linter

--- api/test_linting.py
+++ api/test_linting.py
@@ -1,1 +1,3 @@
-print("This is a very long line of code that exceeds the recommended line length of 79 characters.")
+print(
+    "This is a very long line of code that exceeds the recommended line length of 79 characters."
+)
