687825c7b0048995f6511121de4a8c055451499d
Jesus-sunuc <74077822+Jesus-sunuc@users.noreply.github.com>
2025-03-27 19:07:56 -0600

    api is working

--- client/src/src/features/items/services/itemService.ts
+++ client/src/src/features/items/services/itemService.ts
@@ -2,8 +2,6 @@ import { camel_to_snake } from "../../utils/apiMapper";
 import { axiosClient } from "../../utils/axiosClient";
 import { Item } from "../types/Item";
 
-
-
 export const itemService = {
   async getItems(): Promise<Item[]> {
     const res = await axiosClient.get("/items");
