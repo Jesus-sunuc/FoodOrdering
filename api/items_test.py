9f8f57162b6dae0046b80e3863fb2db8036eeced
zane Clegg <zane.clegg@students.snow.edu>
2025-03-28 12:04:19 -0600

    ran black lint, tests should fail now

--- api/items_test.py
+++ api/items_test.py
@@ -53,6 +53,7 @@ def test_get_all(repo):
 
     assert expected_item not in items
 
+
 def test_endpointnav():
     response = client.get("/")
     assert response.json() == {"message": "Hello from Azure!"}
