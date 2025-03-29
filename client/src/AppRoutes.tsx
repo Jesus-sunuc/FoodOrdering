535090a0eae7ef0086120990ef14288556ef7bda
Jesus-sunuc <74077822+Jesus-sunuc@users.noreply.github.com>
2025-03-29 02:00:24 -0600

    cleaning up

--- client/src/AppRoutes.tsx
+++
@@ -1,8 +0,0 @@
-import { Route, Routes } from "react-router-dom";
-import { App } from "../src/src/App";
-
-export const AppRoutes = () => (
-  <Routes>
-    <Route path="/" element={<App />} />
-  </Routes>
-);
