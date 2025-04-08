ece1a5f5c71c143ebfbfdca458c1a9de9665d68d
Jesus Sunuc <mariano.code4guate@gmail.com>
2025-04-08 16:00:13 -0600

    updates

--- api/main.py
+++ api/main.py
@@ -5,7 +5,6 @@ from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 
-# Set up OpenTelemetry logging
 resource = Resource(attributes={"service.name": "fastapi-backend"})
 provider = LoggerProvider(resource=resource)
 exporter = OTLPLogExporter(
@@ -14,12 +13,10 @@ exporter = OTLPLogExporter(
 )
 provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
 
-# Attach the OTLP logging handler to Python's standard logging
 otel_handler = LoggingHandler(level=logging.INFO, logger_provider=provider)
 logging.getLogger().addHandler(otel_handler)
 logging.getLogger().setLevel(logging.INFO)
 
-# FastAPI App
 from fastapi import APIRouter, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.item_router import router as item_router
@@ -27,20 +24,22 @@ from routes.item_router import router as item_router
 app = FastAPI()
 api_router = APIRouter(prefix="/api")
 
+
 @api_router.get("/")
 def root():
     logging.getLogger(__name__).info("Hit root endpoint")
     return {"message": "Hello from Azure!"}
+
 
 @api_router.get("/health")
 def health_check():
     logging.getLogger(__name__).info("Health check pinged")
     return {"status": "Healthy"}
 
+
 api_router.include_router(item_router)
 app.include_router(api_router)
 
-# CORS configuration
 origins = [
     "http://localhost:4173",
     "http://lunchbox6.duckdns.org",
