0845a0c4372e98e1cd969ef3b7ef32d3ea318acc
Jesus Sunuc <mariano.code4guate@gmail.com>
2025-04-09 10:55:49 -0600

    traces and metrics are implemented to docker compose.

--- api/main.py
+++ api/main.py
@@ -1,28 +1,67 @@
 import logging
 
+from fastapi import APIRouter, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes.item_router import router as item_router
+
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+
+from opentelemetry import trace, metrics
+
 resource = Resource(attributes={"service.name": "fastapi-backend"})
-provider = LoggerProvider(resource=resource)
-exporter = OTLPLogExporter(
-    endpoint="http://otel-collector-service.lunchbox.svc.cluster.local:4317",
+
+# exporter = OTLPLogExporter(
+#     endpoint="http://otel-collector-service.lunchbox.svc.cluster.local:4317",
+#     insecure=True,
+# )
+
+# --- Tracing ---
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer_provider = trace.get_tracer_provider()
+tracer_exporter = OTLPSpanExporter(endpoint="http://otel-collector:4317", insecure=True)
+tracer_provider.add_span_processor(BatchSpanProcessor(tracer_exporter))
+
+# --- Metrics ---
+metric_exporter = OTLPMetricExporter(
+    endpoint="http://otel-collector:4317", insecure=True
+)
+metric_reader = PeriodicExportingMetricReader(metric_exporter)
+metrics.set_meter_provider(
+    MeterProvider(resource=resource, metric_readers=[metric_reader])
+)
+
+# --- Logging ---
+log_exporter = OTLPLogExporter(
+    endpoint="http://otel-collector:4317",
     insecure=True,
 )
-provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
 
-otel_handler = LoggingHandler(level=logging.INFO, logger_provider=provider)
+log_provider = LoggerProvider(resource=resource)
+log_provider.add_log_record_processor(BatchLogRecordProcessor(log_exporter))
+
+otel_handler = LoggingHandler(level=logging.INFO, logger_provider=log_provider)
 logging.getLogger().addHandler(otel_handler)
 logging.getLogger().setLevel(logging.INFO)
 
-
-from fastapi import APIRouter, FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from routes.item_router import router as item_router
-
+# --- FastAPI App ---
 app = FastAPI()
+FastAPIInstrumentor.instrument_app(app)
+RequestsInstrumentor().instrument()
+
 api_router = APIRouter(prefix="/api")
 
 
@@ -40,6 +79,7 @@ def health_check():
 
 api_router.include_router(item_router)
 app.include_router(api_router)
+
 
 origins = [
     "http://localhost:4173",
