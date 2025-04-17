735ecb62ca930b9c20137b53196494afca5ac24a
Jesus Sunuc <mariano.code4guate@gmail.com>
2025-04-16 23:33:40 -0600

    update

--- api/main.py
+++ api/main.py
@@ -106,7 +106,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_origin_regex=r"https://.*\.azurestaticapps\.net$", 
+    allow_origin_regex=r"https://.*\.azurestaticapps\.net$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
