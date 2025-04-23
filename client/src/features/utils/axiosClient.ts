21bf6166fc8e9c1992e216a29d7146c1d8e7b7ad
sarah-strawberries <blue.howrse@gmail.com>
2025-04-23 13:58:19 -0600

    trying to fix url

--- client/src/features/utils/axiosClient.ts
+++ client/src/features/utils/axiosClient.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL;
+const baseURL = "https://lunchbox6.duckdns.org/api"
+//import.meta.env.VITE_API_URL;
 
 export const axiosClient = axios.create({
   baseURL,
