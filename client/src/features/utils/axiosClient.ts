f690ae6f46447d01a01c1c976dc9969853f03574
sarah-strawberries <blue.howrse@gmail.com>
2025-04-23 14:15:30 -0600

    fixed use of env variable for vite api url

--- client/src/features/utils/axiosClient.ts
+++ client/src/features/utils/axiosClient.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://lunchbox6.duckdns.org/api"
-//import.meta.env.VITE_API_URL;
+const baseURL = import.meta.env.VITE_API_URL;
 
 export const axiosClient = axios.create({
   baseURL,
